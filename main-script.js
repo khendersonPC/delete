cbc3166b1714987373ba6a30cd15dbe71780b3c2
Kelly Henderson <20666083+khendersonPC@users.noreply.github.com>
2022-09-12 14:15:49 -0600

    Update main-script.js

--- main-script.js
+++ main-script.js
@@ -2,7 +2,7 @@
 function getImage(){
     //get the element we want to change
     var picSpot = document.getElementById("coolPhoto");
-    picSpot.innerHTML="<img src = 'images/conan.jpeg'/>";
+    picSpot.innerHTML="<img src = 'conan.jpeg'/>";
 }
 
 function hideImage(){
